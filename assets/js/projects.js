e636347e0c364685f9a5e6a61149a1c8b66a7c41
Vinicius Ventura <vinicius__20@hotmail.com>
2024-04-06 16:44:23 -0300

    fix: sort the projects

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -5,6 +5,16 @@ export default [
     url: 'https://restaurant-finder-react-dio.netlify.app/'
   },
   {
+    text: 'Creating a Newsletter form using Vite, React, TypeScript and Tailwind',
+    image: 'newsletter.png',
+    url: 'https://newsletter-form-vite-react.netlify.app/'
+  },
+  {
+    text: "Rocketseat's Next Level Week project, where was created a selective collection website for a city using HTML, CSS and JavaScript. (in progress)",
+    image: 'ecoleta.png',
+    url: 'https://ecoleta-nlw-rocketseat.netlify.app/'
+  },
+  {
     text: 'Recreating the Instagram login page using HTML and CSS.',
     image: 'instagram.png',
     url: 'https://insta-login-page-clone.netlify.app/'
@@ -13,11 +23,6 @@ export default [
     text: 'Recreating the Twitter login page using HTML and CSS.',
     image: 'twitter.png',
     url: 'https://twitter-login-clone.netlify.app/'
-  },
-  {
-    text: "Rocketseat's Next Level Week project, where was created a selective collection website for a city using HTML, CSS and JavaScript. (in progress)",
-    image: 'ecoleta.png',
-    url: 'https://ecoleta-nlw-rocketseat.netlify.app/'
   },
   {
     text: 'The project purpose is recreate the Netflix home page, using HTML, CSS, JavaScript and JQuery',
@@ -34,9 +39,4 @@ export default [
     image: 'tips.png',
     url: 'https://calculadora-gorjeta-js-onebitcode.netlify.app/'
   },
-  {
-    text: 'Creating a Newsletter form using Vite, React, TypeScript and Tailwind',
-    image: 'newsletter.png',
-    url: 'https://newsletter-form-vite-react.netlify.app/'
-  }
 ];
