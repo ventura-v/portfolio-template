83945ec9b97b26c505ca7f8b7cf109874115822d
Vinicius Ventura <vinicius__20@hotmail.com>
2024-04-06 11:46:46 -0300

    fix: insert links for the projects

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -2,22 +2,27 @@ export default [
   {
     text: 'Recreating the Instagram login page using HTML and CSS.',
     image: 'instagram.png',
+    url: 'https://insta-login-page-clone.netlify.app/'
   },
   {
     text: 'Recreating the Twitter login page using HTML and CSS.',
     image: 'twitter.png',
+    url: 'https://twitter-login-clone.netlify.app/'
   },
   {
     text: "Rocketseat's Next Level Week project, where was created a selective collection website for a city using HTML, CSS and JavaScript. (in progress)",
     image: 'ecoleta.png',
+    url: 'https://ecoleta-nlw-rocketseat.netlify.app/'
   },
   {
     text: 'The project purpose is recreate the Netflix home page, using HTML, CSS, JavaScript and JQuery',
     image: 'netflix.png',
+    url: 'https://netflix-clone-home.netlify.app/'
   },
   {
     text: 'Recreating the famous snake game in JavaScript language, through the practical challenge in Digital Innovation One plataform. (in progress)',
     image: 'snake.png',
+    url: 'https://snake-game-js-html-css.netlify.app/'
   },
   {
     text: 'Creating a tip calculator, using HTML, CSS and JavaScript.',
